38ab5678ee5f1a873770444a5d662245ee7e02fb
Chris Ruppel <ruppel@reliefweb.int>
2024-02-06 11:46:56 +0100

    fix(ux): insert question into DOM before ajax returns
    Moved the DOM manipulation function into a common library
    so that both chat.popup and chat.form can use the same
    code.
    
    Refs: RW-857

--- components/chat-form/chat-form.js
+++ components/chat-form/chat-form.js
@@ -7,9 +7,55 @@
 
   Drupal.behaviors.ochaAiChatForm = {
     attach: function (context, settings) {
-      // Scroll to bottom of chat contents container.
       var chatContainer = document.querySelector('[data-drupal-selector="edit-chat"] .fieldset-wrapper');
+
+      // Scroll to bottom of chat container. We can't use smooth scrolling to
+      // create a convincing effect without storing and calculating the position
+      // from before the ajax submission, so we immediately set it to the bottom
+      // so that any new content is prominently visible neat the text input.
       chatContainer.scrollTop = chatContainer.scrollHeight;
+
+      // Custom actions when submit is pressed. Each time ajax finishes, the whole
+      // chat history will be re-rendered so we can temporarily inject whatever we
+      // like and it will get cleaned up.
+      var submitButton = document.querySelector('[data-drupal-selector="edit-submit"]');
+      // TODO: will this work on mobile or do we need to attach the listener to
+      // separate events? e.g. touchend
+      submitButton.addEventListener('mousedown', event => {
+        var chatContainer = document.querySelector('[data-drupal-selector="edit-chat"] .fieldset-wrapper');
+        var chatResult = Drupal.behaviors.ochaAiChatUtils.createElement('div', {
+          'class': 'ocha-ai-chat-result',
+        }, {});
+        var questionDl = Drupal.behaviors.ochaAiChatUtils.createElement('dl', {
+          'class': 'chat',
+        }, {});
+        var questionWrapper = Drupal.behaviors.ochaAiChatUtils.createElement('div', {
+          'class': 'chat__q chat__q--loading',
+        }, {});
+        var questionDt = Drupal.behaviors.ochaAiChatUtils.createElement('dt', {
+            'class': 'visually-hidden',
+          }, 'Question'
+        );
+        var questionValue = document.querySelector('[data-drupal-selector="edit-question"]').value;
+        var questionDd = Drupal.behaviors.ochaAiChatUtils.createElement('dd', {}, questionValue);
+
+        // Prep all the DOM nodes for insertion.
+        questionWrapper.append(questionDt);
+        questionWrapper.append(questionDd);
+        questionDl.append(questionWrapper);
+        chatResult.append(questionDl);
+
+        // Introduce a small delay before question gets inserted into DOM.
+        setTimeout(() => {
+          chatContainer.append(chatResult);
+
+          // In this instance we use smooth scrolling. It won't be smooth unless
+          // the continer can be scrolled to begin with, so in practice the
+          // first one never has a smooth scroll.
+          chatContainer.scrollTo({top: chatContainer.scrollHeight, behavior: 'smooth'});
+        }, 200);
+      });
     },
   };
+
 })();
--- components/chat-popup/chat-popup.js
+++ components/chat-popup/chat-popup.js
@@ -1,50 +1,5 @@
 (function () {
-
   'use strict';
-
-  /**
-   * Create a DOM element with the given attributes and content.
-   *
-   * @param {String} tag
-   *   The element tag.
-   * @param {Object} attributes
-   *   Map of attribute names and values.
-   * @param {String|Element|Array} content
-   *   Either a text, an DOM element or an array of DOM elements.
-   *
-   * @return Element
-   *   The created DOM element.
-   */
-  function createElement(tag, attributes, content) {
-    const element = document.createElement(tag);
-    if (typeof attributes === 'object') {
-      for (const attribute in attributes) {
-        if (attributes.hasOwnProperty(attribute)) {
-          let value = attributes[attribute];
-          element.setAttribute(attribute, Array.isArray(value) ? value.join(' ') : value);
-        }
-      }
-    }
-    switch (typeof content) {
-      case 'string':
-        element.appendChild(document.createTextNode(content));
-        break;
-
-      case 'object':
-        // Assume it's is DOM element.
-        if (content.nodeType) {
-          element.appendChild(content);
-        }
-        // Assume it's a list of DOM elements.
-        else if (typeof content.length !== 'undefined') {
-          for (var i = 0, l = content.length; i < l; i++) {
-            element.appendChild(content[i]);
-          }
-        }
-        break;
-    }
-    return element;
-  }
 
   function processPopup(popup) {
     // Mark the popup as processed so the following code runs only once.
@@ -56,7 +11,7 @@
 
     const link = popup.querySelector('a');
 
-    const iframe = createElement('iframe', {
+    const iframe = Drupal.behaviors.ochaAiChatUtils.createElement('iframe', {
       'class': 'ocha-ai-chat-chat-popup__iframe',
       'data-src': link.href
     });
@@ -72,10 +27,10 @@
     link.replaceWith(iframe);
 
     // Replace the link with a button to open the chat window.
-    const openButton = createElement('button', {
+    const openButton = Drupal.behaviors.ochaAiChatUtils.createElement('button', {
       'type': 'button',
       'class': 'data-ocha-ai-chat-chat-popup__button-open'
-    }, createElement('span', {
+    }, Drupal.behaviors.ochaAiChatUtils.createElement('span', {
       'class': 'data-ocha-ai-chat-chat-popup__button-open__label visually-hidden'
     }, Drupal.t('Open chat')));
 
@@ -94,10 +49,10 @@
 
     // Create a separate close button that sits inside the <section> and can
     // be more easily positioned in all viewport sizes.
-    const closeButton = createElement('button', {
+    const closeButton = Drupal.behaviors.ochaAiChatUtils.createElement('button', {
       'type': 'button',
       'class': 'data-ocha-ai-chat-chat-popup__button-close'
-    }, createElement('span', {
+    }, Drupal.behaviors.ochaAiChatUtils.createElement('span', {
       'class': 'data-ocha-ai-chat-chat-popup__button-close__label visually-hidden'
     }, Drupal.t('Close chat')));
 
@@ -113,7 +68,7 @@
   /**
    * Drupal behavior for the chat popup.
    */
-  Drupal.behaviors.ochaAiChat = {
+  Drupal.behaviors.ochaAiChatPopup = {
     attach: function (context, settings) {
       // Get the "Ask ReliefWeb" popup.
       const popup = context.querySelector('[data-ocha-ai-chat-chat-popup]');
