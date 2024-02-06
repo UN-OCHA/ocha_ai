c025fc97a64b6a637ed07cdb069c361431337ac1
Chris Ruppel <ruppel@reliefweb.int>
2024-02-06 10:20:11 +0100

    fix(ux): scroll to bottom of chat container when new content loads
    Refs: RW-857

--- components/chat-form/chat-form.js
+++ components/chat-form/chat-form.js
@@ -1,5 +1,15 @@
 (function () {
   'use strict';
 
+  // Initialize. We do this outside Drupal.behaviors because it doesn't need to
+  // run each time ajax gets called.
   window.parent.postMessage('ready', window.origin);
+
+  Drupal.behaviors.ochaAiChatForm = {
+    attach: function (context, settings) {
+      // Scroll to bottom of chat contents container.
+      var chatContainer = document.querySelector('[data-drupal-selector="edit-chat"] .fieldset-wrapper');
+      chatContainer.scrollTop = chatContainer.scrollHeight;
+    },
+  };
 })();
