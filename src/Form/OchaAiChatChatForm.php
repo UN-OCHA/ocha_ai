bf5bc8b4d449ab080e6fcfa4c46833611a5496c9
Chris Ruppel <ruppel@reliefweb.int>
2024-02-07 10:14:10 +0100

    test: fine, we can always look in git for this code

--- src/Form/OchaAiChatChatForm.php
+++ src/Form/OchaAiChatChatForm.php
@@ -107,44 +107,6 @@ class OchaAiChatChatForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state, ?bool $popup = NULL): array {
     $defaults = $this->ochaAiChat->getSettings();
 
-    /*
-    // Display the form instruction config.
-    if (!empty($defaults['form']['instructions']['value'])) {
-      $hide = $form_state->getValue([
-        'instructions',
-        'content',
-        'hide',
-      ], $this->database
-        ->select('ocha_ai_chat_preferences', 't')
-        ->fields('t', ['hide_instructions'])
-        ->condition('t.uid', $this->currentUser->id())
-        ->execute()
-        ?->fetchField()
-      );
-      $form['instructions'] = [
-        '#type' => 'details',
-        '#prefix' => '<div id="ocha-ai-chat-instructions" class="ocha-ai-chat-chat-form__instructions">',
-        '#suffix' => '</div>',
-        '#title' => $this->t('Instructions'),
-        '#open' => !empty($hide) ? FALSE : TRUE,
-        '#tree' => TRUE,
-      ];
-      $form['instructions']['content']['hide'] = [
-        '#type' => 'checkbox',
-        '#title' => $this->t('Do not show instructions anymore'),
-        '#default_value' => !empty($hide),
-        '#limit_validation_errors' => [
-          ['instructions', 'content', 'hide'],
-        ],
-        '#ajax' => [
-          'callback' => [$this, 'hideInstructions'],
-          'wrapper' => 'ocha-ai-chat-instructions',
-          'disable-refocus' => TRUE,
-        ],
-      ];
-    }
-    */
-
     // Add the source widget.
     $form = $this->ochaAiChat->getSourcePlugin()->getSourceWidget($form, $form_state, $defaults);
     if (isset($form['source'])) {
