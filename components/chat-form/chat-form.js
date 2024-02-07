de6b1c48cbc424618bc2d3aa0231b21ad7712dc9
Chris Ruppel <ruppel@reliefweb.int>
2024-02-07 09:27:34 +0100

    fix(ux): smooth scroll all chat updates starting with second question
    Until the chat container is full of content, the scrolling can't
    happen at all, but this works well once the box is filled.
    
    Refs: RW-857

--- components/chat-form/chat-form.js
+++ components/chat-form/chat-form.js
@@ -1,6 +1,10 @@
 (function () {
   'use strict';
 
+  // These variables need to survive in between executions of Drupal's `attach`
+  // method so we instantiate them outside the Behavior itself.
+  var oldScrollHeight;
+
   // Initialize. We do this outside Drupal.behaviors because it doesn't need to
   // run each time ajax gets called.
   window.parent.postMessage('ready', window.origin);
@@ -8,20 +12,26 @@
   Drupal.behaviors.ochaAiChatForm = {
     attach: function (context, settings) {
       var chatContainer = document.querySelector('[data-drupal-selector="edit-chat"] .fieldset-wrapper');
-
-      // Scroll to bottom of chat container. We can't use smooth scrolling to
-      // create a convincing effect without storing and calculating the position
-      // from before the ajax submission, so we immediately set it to the bottom
-      // so that any new content is prominently visible neat the text input.
-      chatContainer.scrollTop = chatContainer.scrollHeight;
-
-      // Custom actions when submit is pressed. Each time ajax finishes, the whole
-      // chat history will be re-rendered so we can temporarily inject whatever we
-      // like and it will get cleaned up.
       var submitButton = document.querySelector('[data-drupal-selector="edit-submit"]');
-      // TODO: will this work on mobile or do we need to attach the listener to
-      // separate events? e.g. touchend
-      submitButton.addEventListener('mousedown', event => {
+
+      // Do some calculations to decide where to start our smooth scroll.
+      if (oldScrollHeight) {
+        var chatHeight = chatContainer.getBoundingClientRect().height;
+        var smoothScrollStart = oldScrollHeight - chatHeight;
+
+        // Jump to where the bottom of the previous container was before the DOM
+        // got updated. From there, we smooth-scroll to the bottom.
+        chatContainer.scrollTo({top: smoothScrollStart, behavior: 'instant'});
+        chatContainer.scrollTo({top: chatContainer.scrollHeight, behavior: 'smooth'});
+      }
+      else {
+        chatContainer.scrollTo({top: chatContainer.scrollHeight, behavior: 'smooth'});
+      }
+
+      // Upate UI when submit is pressed. Each time ajax finishes, the
+      // whole chat history will be re-inserted into the DOM. That means we can
+      // temporarily inject whatever we like and it will get cleaned up for us.
+      function chatSend (event) {
         var chatContainer = document.querySelector('[data-drupal-selector="edit-chat"] .fieldset-wrapper');
         var chatResult = Drupal.behaviors.ochaAiChatUtils.createElement('div', {
           'class': 'ocha-ai-chat-result',
@@ -33,9 +43,8 @@
           'class': 'chat__q chat__q--loading',
         }, {});
         var questionDt = Drupal.behaviors.ochaAiChatUtils.createElement('dt', {
-            'class': 'visually-hidden',
-          }, 'Question'
-        );
+          'class': 'visually-hidden',
+        }, 'Question');
         var questionValue = document.querySelector('[data-drupal-selector="edit-question"]').value;
         var questionDd = Drupal.behaviors.ochaAiChatUtils.createElement('dd', {}, questionValue);
 
@@ -53,8 +62,17 @@
           // the continer can be scrolled to begin with, so in practice the
           // first one never has a smooth scroll.
           chatContainer.scrollTo({top: chatContainer.scrollHeight, behavior: 'smooth'});
+
+          // Store the scroll position so that we can attempt to smooth-scroll
+          // when the form reloads with new data attached.
+          oldScrollHeight = chatContainer.scrollHeight;
         }, 200);
-      });
+      };
+
+      // All the input modes!
+      submitButton.addEventListener('mousedown', chatSend);
+      submitButton.addEventListener('keydown', chatSend);
+      submitButton.addEventListener('touchend', chatSend);
     },
   };
 
