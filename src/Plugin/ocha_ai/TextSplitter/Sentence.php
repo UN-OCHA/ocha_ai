d23ee58d818e353365066800f3889bd1ac03471b
Peter Droogmans (attiks) <peter@attiks.com>
2024-04-25 16:46:30 +0200

    feat: Split long sentences with overlap
    Refs: RW-944

--- src/Plugin/ocha_ai/TextSplitter/Sentence.php
+++ src/Plugin/ocha_ai/TextSplitter/Sentence.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\ocha_ai\Plugin\ocha_ai\TextSplitter;
 
+use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\ocha_ai\Attribute\OchaAiTextSplitter;
@@ -38,7 +39,27 @@ class Sentence extends TextSplitterPluginBase {
       foreach (preg_split('/\n+/u', $paragraph, -1, \PREG_SPLIT_NO_EMPTY) as $sentence) {
         $sentence = trim($sentence);
         if (!empty($sentence)) {
-          $sentences[] = $sentence;
+          if (mb_strlen($sentence) > $length) {
+            // First sentence is full length.
+            $new_sentence = Unicode::truncate($sentence, $length, TRUE);
+            $sentences[] = $new_sentence;
+
+            // Grab overlap.
+            $str_overlap = trim(strrev(Unicode::truncate(strrev($new_sentence), $overlap, TRUE)));
+
+            // Shorten string.
+            $sentence = trim(mb_substr($sentence, mb_strlen($new_sentence)));
+
+            while (mb_strlen($sentence) > 0) {
+              $new_sentence = Unicode::truncate($sentence, $length - $overlap, TRUE);
+              $sentences[] = $str_overlap . ' ' . $new_sentence;
+              $str_overlap = trim(strrev(Unicode::truncate(strrev($new_sentence), $overlap, TRUE)));
+              $sentence = trim(mb_substr($sentence, mb_strlen($new_sentence)));
+            }
+          }
+          else {
+            $sentences[] = $sentence;
+          }
         }
       }
 
