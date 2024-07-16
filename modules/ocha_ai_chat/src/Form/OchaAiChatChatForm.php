18b75b7a78b02f3d258367f08147229ec227e92d
orakili <github@orakili.net>
2024-07-16 06:22:26 +0000

    fix: hide reference when the answer is not successful
    Refs: RW-1022

--- modules/ocha_ai_chat/src/Form/OchaAiChatChatForm.php
+++ modules/ocha_ai_chat/src/Form/OchaAiChatChatForm.php
@@ -424,7 +424,7 @@ class OchaAiChatChatForm extends FormBase {
           'id' => $data['id'],
           'question' => $question,
           'answer' => $data['answer'],
-          'references' => $references,
+          'references' => $data['status'] === 'success' ? $references : [],
         ];
 
         $form_state->setValue('history', json_encode($history));
